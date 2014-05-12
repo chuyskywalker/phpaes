aaa9009161304463342dd42b76f91c4f1ab7459b
chuyskywalker <github.com@jrm.cc>
2014-05-11 18:43:25 -0700

    Fix 5.3 support which uses a boolean instead of bitmask for 3rd option

--- src/AES_CBC_OpenSSL.php
+++ src/AES_CBC_OpenSSL.php
@@ -17,20 +17,25 @@ class AES_CBC_OpenSSL extends AES {
     /** @var string */
     private $aesmode = '';
 
+    private $rawoption;
+
     public function setKey($key) {
         parent::setKey($key);
         // Transform the key into the bit size and set the openssl mode string
         $this->aesmode = 'aes-'.(8*strlen($key)).'-cbc';
+        // in 5.3 the 3rd option to these calls was a boolean for raw/not raw, but became a bitmask in 5.4
+        // pick the right variant like this:
+        $this->rawoption = defined('OPENSSL_RAW_DATA') ? OPENSSL_RAW_DATA : true;
     }
 
     /** @inheritdoc */
     public function encrypt($text) {
-        return openssl_encrypt($text, $this->aesmode, $this->getKey(), OPENSSL_RAW_DATA, $this->getIv());
+        return openssl_encrypt($text, $this->aesmode, $this->getKey(), $this->rawoption, $this->getIv());
     }
 
     /** @inheritdoc */
     public function decrypt($cipherText) {
-        return openssl_decrypt($cipherText, $this->aesmode, $this->getKey(), OPENSSL_RAW_DATA, $this->getIv());
+        return openssl_decrypt($cipherText, $this->aesmode, $this->getKey(), $this->rawoption, $this->getIv());
     }
 
 }
